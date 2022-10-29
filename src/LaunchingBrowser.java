9fc16c4ee2577de4eb15665e3bddbd75a3355285
Gunduzhazal <gunduzhazal90@gmail.com>
2022-10-29 16:47:53 -0500

    Initial commit

--- src/LaunchingBrowser.java
+++ src/LaunchingBrowser.java
@@ -9,7 +9,27 @@ public class LaunchingBrowser {
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.google.com/");
+        String path = "https://www.google.com/";
+        driver.get(path);
+
+        String URL = driver.getCurrentUrl();
+
+        System.out.println(path);
+        System.out.println(URL);
+
+        String title = driver.getTitle();
+
+        if(path.equalsIgnoreCase(URL)) {
+            System.out.println("Path matches URL");
+        } else {
+            System.out.println("Path doesn't match URL");
+        }
+
+        if(title.equalsIgnoreCase("Google")) {
+            System.out.println("Title matches");
+        } else {
+            System.out.println("Title doesn't match");
+        }
 
         driver.quit();
     }
