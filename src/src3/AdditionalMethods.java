76671f6e50dd12abcce6c16a28a061417c9d2f9c
Gunduzhazal <gunduzhazal90@gmail.com>
2022-11-02 23:24:09 -0500

    Initial commit

---
+++ src/src3/AdditionalMethods.java
@@ -0,0 +1,27 @@
+package src3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AdditionalMethods {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","driver/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://syntaxprojects.com/basic-radiobutton-demo.php");
+
+        WebElement btn = driver.findElement(By.cssSelector("button#buttoncheck"));
+
+        String text = btn.getText();
+        System.out.println("the text in the button is : "+text);
+
+        String idValue = btn.getAttribute("id");
+        System.out.println("the value of the attribute id is :"+idValue);
+
+    }
+}
